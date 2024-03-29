050171f14b19387a00872601c4498c0fda8349aa
Stu Mason <stu@stuartmason.co.uk>
2024-03-29 12:51:13 +0000

    club-v1

--- Scrappy/handler.py
+++ Scrappy/handler.py
@@ -1,73 +1,27 @@
 import json
 import traceback
-from bs4 import BeautifulSoup
-from chrome_service import ChromeService
+from platforms.clubv1.tee_times import handle as handle_clubv1
 
 
 def handle(event, context):
     try:
-        endpoint = event["endpoint"]
-        content = scrape_website(endpoint)
-        # save content to a file
-        print(content)
-        exit()
-        soup = BeautifulSoup(content, features="html.parser")
-        links = get_tee_times(soup, endpoint)
-        # reduce links array to 10
-        links = links[:10]
-        for link in links:
-            article = scrape_website(link["href"])
-            soup = BeautifulSoup(article, features="html.parser")
-            article = soup.find("article")
-            if article:
-                link["article"] = article.get_text()
-
-        return links
-
-        # return {
-        #     "statusCode": 200,
-        #     "body": json.dumps({"data": content}),
-        # }
-    except Exception:
+        """
+        {
+            "platform": "clubv1",
+            "url": "https://www.example.com/lkjlkj",
+            "date": "2021-12-25"
+        }
+        """
+        platform = event.get("platform")
+        if platform == "clubv1":
+            return handle_clubv1(event)
+        else:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"message": "Unsupported platform"}),
+            }
+    except Exception as e:
         error_message = "An error occurred while processing the request."
         print(error_message)
         print(traceback.format_exc())
-        return {
-            "statusCode": 500,
-            "body": json.dumps({"error": error_message})
-        }
-
-
-def scrape_website(endpoint):
-    chrome = ChromeService()
-    chrome.get_url(endpoint)
-    source = chrome.get_loaded_source()
-    chrome.close()
-    return source
-
-
-def get_tee_times(content, endpoint):
-    tee_times = []
-
-    for time in content.find_all('div', content.find_all("div", class_="tee available")):
-        print(time)
-        exit()
-        text = time.get_text()
-        href = time['href']
-
-        if len(text.split()) < 4:
-            continue
-
-        skip = ["Audio", "Video", "our", "BBC"]
-        if any(word in text for word in skip):
-            continue
-
-        if not href.startswith(endpoint):
-            href = endpoint + href
-
-        tee_times.append({
-            "text": text,
-            "href": href
-        })
-
-    return tee_times
+        return {"statusCode": 500, "body": json.dumps({"message": error_message})}
---
+++ Scrappy/platforms/clubv1/tee_times.py
@@ -0,0 +1,56 @@
+import json
+import traceback
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urlunparse, urlencode
+from chrome_service import ChromeService
+
+def handle(event):
+    try:
+        url = event.get("url")
+        date = event.get("date")
+        parsed_url = urlparse(url)
+        root_url = urlunparse((parsed_url.scheme, parsed_url.netloc, '', '', '', ''))
+        query_params = {'date': date}
+        endpoint = root_url + parsed_url.path + "?" + urlencode(query_params)
+        content = scrape_website(endpoint)
+        soup = BeautifulSoup(content, features="html.parser")
+        tee_times = get_tee_times(soup, root_url)
+        return {
+            "statusCode": 200,
+            "body": json.dumps(tee_times)
+        }
+    except Exception as e:
+        error_message = "An error occurred while handling clubv1 platform request."
+        print(error_message)
+        print(traceback.format_exc())
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": error_message})
+        }
+
+def scrape_website(endpoint):
+    chrome = ChromeService()
+    chrome.get_url(endpoint)
+    source = chrome.get_loaded_source()
+    chrome.close()
+    return source
+
+def get_tee_times(content, root_url):
+    tee_times = []
+    for available_tees in content.find_all('div', class_='tee available'):
+        tee_time = available_tees.get('data-teetime')
+        cost_per_ball = None
+        ball_1_div = available_tees.find('div', class_='price ball-1')
+        if ball_1_div:
+            value_div = ball_1_div.find('div', class_='value')
+            if value_div:
+                cost_per_ball = value_div.text.strip()
+
+        tee_times.append({
+            "time": tee_time.split(" ")[1],
+            "date": tee_time.split(" ")[0],
+            "bookingUrl": root_url + available_tees.find("a")["href"],
+            "cost_per_ball": cost_per_ball
+        })
+
+    return tee_times
